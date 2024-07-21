25f40025b04eb22a4c514db8920dc25148f5e103
leeuihyoun <dasuu3@naver.com>
2024-07-21 09:03:51 +0900

    지도 구현

---
+++ next-weather-app/src/kakao-map/kakao-map.tsx
@@ -0,0 +1,15 @@
+import { Map, MapMarker } from "react-kakao-maps-sdk"
+
+function KakaoMap() {
+  
+  return (
+    <Map
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{ width: "100%", height: "360px" }}
+    >
+      
+    </Map>
+  )
+}
+
+export default KakaoMap
--- next-weather-app/src/pages/_document.tsx
+++ next-weather-app/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script 
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=979da26f862ae35c7e39fc71639593f5&libraries=services,clusterer&autoload=false"
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
   );
