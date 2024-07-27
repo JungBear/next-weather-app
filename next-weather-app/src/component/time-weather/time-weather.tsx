3969aee1fe20a742c8352686ba198f754a3449c3
ddabbi <suminkk04@gmail.com>
2024-07-27 12:45:38 +0900

    0727 css추가/수정

--- next-weather-app/src/component/time-weather/time-weather.tsx
+++ next-weather-app/src/component/time-weather/time-weather.tsx
@@ -1,28 +1,23 @@
 import { useEffect, useState } from "react";
+import styles from './time-weather.module.css'
 
 
 export default function TimeWeather(){
-    const [ViewList, setViewList] = useState();
-    const [ViewChart, setViewChart] = useState();
-    const [ViewTable, setViewTable] = useState();
+    const [ViewList, setViewList] = useState(true);
+    const [ViewChart, setViewChart] = useState(false);
+    const [ViewTable, setViewTable] = useState(false);
 
     const Today = new Date();
-
-    const endDay = new Date(Today.getDay());
-    const endDate = new Date(Today.getTime() + 2 * 24 * 60 * 60 * 1000); // 현재 시각으로부터 2일 후의 시각을 계산합니다.
-
+    const endDate = new Date(Today.getTime() + 2 * 24 * 60 * 60 * 1000); 
+    // 현재 시각으로부터 2일 후의 시각을 계산합니다.
     const timeList = [];
-    
     let currentTime = Today;
-
 
     
     while (currentTime <= endDate) {
     timeList.push(currentTime.toLocaleString());
     currentTime = new Date(currentTime.getTime() + 1 * 60 * 60 * 1000);
     }
-
-    
 
     // 임시데이터
     const weatherData = {
@@ -36,82 +31,145 @@ export default function TimeWeather(){
         humidity: 70
       };
 
+      // 이렇게 돌려도 정말 괜찮은가??
+      function handleWeatherIcon(weather: string){
+        if(weather === '맑음'){
+            return(
+                <img src="https://www.weather.go.kr/w/resources/icon/DY@64/A/Light/DB01.png"></img>
+            )
+        }else if(weather === '구름많음'){
+            return(
+                <img src="https://www.weather.go.kr/w/resources/icon/DY@64/A/Light/DB03.png"></img>
+            )
+        }else if(weather === '흐림'){
+            return(
+                <img src="https://www.weather.go.kr/w/resources/icon/DY@64/A/Light/DB04_N.png"></img>
+            )
+        }else if(weather === '비'){
+            return(
+                <img src="https://www.weather.go.kr/w/resources/icon/DY@64/A/Light/DB05.png"></img>
+            )
+        }
+      }
+
+      function hanldeWindIcon(){
+        if(weatherData.windDirection === '남서'){
+            return(
+                <img src="https://www.weather.go.kr/w/resources/icon/ic_wd_48x.png" 
+                style={{transform: 'rotate(135deg)', width: '30px', height: '30px'}}/>
+            )
+        }
+      }
+    
 
     return(
         <div>
-            <h3>시간별 예보</h3>
-            <button></button>
-            <button></button>
-            <button></button>
+            <div className={styles.TimeNavbar}>
+                <h3>시간별 예보</h3>
+                <div className={styles.Navbtn}>
+                    <button onClick={()=>{ setViewList(true); setViewChart(false); setViewTable(false); }}>List</button>
+                    <button onClick={()=>{ setViewList(false); setViewChart(true); setViewTable(false); }}>Chart</button>
+                    <button onClick={()=>{ setViewList(false); setViewChart(false); setViewTable(true); }}>Table</button> 
+                </div>    
+            </div>
             
             {/* 일반 */}
-            <div>
-               {
-                ViewList && timeList?.map((item, index) =>{
-                    return(
-                        <div>
-                            <ul>
-                                <li>시각</li>
-                                <li>날씨</li>
-                                <li>기온</li>
-                                <li>체감온도</li>
-                                <li>강수량(mm)</li>
-                                <li>강수확률</li>
-                                <li>바람(m/s)</li>
-                                <li>습도</li>
-                            </ul>
-                            {`${index}시`}
-                            {weatherData.weather}
-                            {weatherData.temperature}
-                            {weatherData.feelsLike}
-                            {weatherData.precipitation}
-                            {weatherData.precipitationProbability}
-                            {weatherData.windSpeed}
-                            {weatherData.humidity}
-                        </div>
-                    )
-                })
-               }
-            </div>
-            {/* 그래프 형태 */}
-            <div>
-                {
-                    ViewChart && timeList?.map((item, index) =>{
+            {
+                ViewList &&(
+                    //<div>{`${Today}`} 최저 - / 최고 -℃</div>
+                    <div className={styles.ViewList}>
                         <ul>
                             <li>시각</li>
                             <li>날씨</li>
                             <li>기온</li>
-                            <li>강수량(mm)</li>
+                            <li>체감온도</li>
+                            <li>강수량<br/>(mm)</li>
                             <li>강수확률</li>
-                            <li>풍향풍속(m/s)</li>
+                            <li>바람<br/>(m/s)</li>
+                            <li>습도</li>
+                            <li>폭염영향</li>
+                        </ul>
+                        
+                    {
+                        timeList?.map((item, index) =>{
+                            return(
+                                <div className={styles.ViewListDetail}>
+                                    <ul>
+                                        <li>{`${index%24}시`}</li>
+                                        <li>{handleWeatherIcon(weatherData.weather)}</li>
+                                        <li>{`${weatherData.temperature}℃`}</li>
+                                        <li>{`${weatherData.feelsLike}℃`}</li>
+                                        <li className={styles.precipitation}>{weatherData.precipitation}</li>
+                                        <li>{`${weatherData.precipitationProbability}%`}</li>
+                                        <li>
+                                            <span>{hanldeWindIcon()}</span> <br/>
+                                            <span>{weatherData.windSpeed}</span>
+                                        </li>
+                                        <li>{`${weatherData.humidity}%`}</li>
+                                    </ul>
+
+                                </div>
+                            )
+                        })
+                    }
+                    </div>
+                )
+            }
+            
+            {/* 그래프 형태 */}
+            {
+                ViewChart&&(
+                    <div className={styles.ViewChart}>
+                        <ul>
+                            <li>시각</li>
+                            <li>날씨</li>
+                            <li>기온</li>
+                            <li>강수량<br/>(mm)</li>
+                            <li>강수확률</li>
+                            <li>풍향풍속<br/>(m/s)</li>
                             <li>습도</li>
                         </ul>
-                        {weatherData.temperature}
-                        {weatherData.precipitation}
-                        {weatherData.precipitationProbability}
-                        {weatherData.windSpeed}
-                        {weatherData.humidity}
-                    })
-                }
-            </div>
+                    {
+                        ViewChart === true && timeList?.map((item, index) =>{
+                            return(
+                                <div className={styles.ViewChartDetail}>
+                                    <ul>
+                                        <li>{`${index%24}시`}</li>
+                                        <li>{handleWeatherIcon(weatherData.weather)}</li>
+                                        <li>{`${weatherData.temperature}℃`}</li>
+                                        <li>{weatherData.precipitation}</li>
+                                        <li>{`${weatherData.precipitationProbability}%`}</li>
+                                        <li>{weatherData.windSpeed}</li>
+                                        <li>{`${weatherData.humidity}%`}</li>
+                                    </ul>
+                                </div>    
+                            )
+                        }) 
+                    }
+                    </div>
+                )
+            }
+
             {/* 테이블 형태 */}
             <div>
                 {
-                    ViewTable && timeList.map((item,index) =>{
-                        <div>
-                            <h3>요일</h3>
-                            <ul>
-                                <li>시각</li>
-                                <li>날씨</li>
-                                <li>기온(체감)</li>
-                                <li>강수량</li>
-                                <li>강수확률</li>
-                                <li>바람</li>
-                                <li>습도</li>
-                                <li>폭염영향</li>
-                            </ul>
-                            
-                        </div>
+                    ViewTable === true && timeList.map((item,index) =>{
+                        return(
+                            <div>
+                                <h3>요일</h3>
+                                <ul>
+                                    <li>시각</li>
+                                    <li>날씨</li>
+                                    <li>기온(체감)</li>
+                                    <li>강수량</li>
+                                    <li>강수확률</li>
+                                    <li>바람</li>
+                                    <li>습도</li>
+                                    <li>폭염영향</li>
+                                </ul>                      
+                            </div>
+                        )
+                        
                     })
                 }
             </div>
